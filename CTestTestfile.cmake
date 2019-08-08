7365fcb7eae4f5504e1ef48d0bfc4745bcc91e9c
unknown <webstorage119@gmail.com>
2019-08-08 22:18:32 +0900

    libssh2-1.8.0-VC2008-MT-32-64-SRC

---
+++ CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/MyProjects/libssh2-1.8.0-VC2008-MT-32-SRC-CMAKE
+# Build directory: C:/Projects/MyProjects/libssh2-1.8.0-VC2008-MT-32-SRC-CMAKE
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("example")
+subdirs("tests")
+subdirs("docs")
---
+++ docs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/MyProjects/libssh2-1.8.0-VC2008-MT-32-SRC-CMAKE/docs
+# Build directory: C:/Projects/MyProjects/libssh2-1.8.0-VC2008-MT-32-SRC-CMAKE/docs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
